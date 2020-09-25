0316be05b2b1b83a8ce6989149a436f17b6d2c4b
OmerHagage <omerhagage@gmail.com>
2020-09-25 22:39:19 +0300

    4 In A Row: full commit

--- fourInARow/game.py
+++ fourInARow/game.py
@@ -1,10 +1,3 @@
-###############################################################
-# FILE : ex12.py
-# WRITER : Omer Hagage , omerhagage , 313525388
-# Yuval ussishkin, yuvalu, 208489963
-# EXERCISE : intro2cs ex12 2018-2019
-# DESCRIPTION : connect 4
-###############################################################
 
 from fourInARow.board import Board
 
